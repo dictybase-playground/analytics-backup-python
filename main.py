563a12164c5885bf7df05a5b0de1c2663bc5a2eb
wildlifehexagon <hartline.eric@gmail.com>
2021-06-29 17:55:09 -0500

    feat: continue fetching results when there is a nextPageToken

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from analytics.cmd import parse_cmdline
 from analytics.service import initialize_analyticsreporting
 from analytics.fetch import get_report
@@ -19,9 +20,22 @@ def main():
     analytics = initialize_analyticsreporting()
     response = get_report(analytics, params)
     data = parse_response(response)
-    # pageToken = response['reports'][0].get('nextPageToken')
     headers = get_csv_headers(response)
+    pageToken = response['reports'][0].get('nextPageToken')
+
+    while pageToken != None:
+        print(
+            f"fetching next batch of results starting with page token {pageToken}")
+        analytics = initialize_analyticsreporting()
+        response = get_report(analytics, params, str(int(pageToken)))
+        pageToken = response['reports'][0].get('nextPageToken')
+        newData = parse_response(response)
+        data = pd.concat([data, newData])
+
+    data = data.reset_index(drop=True)
+    print("finished fetching results")
     save_response(data, headers, params.outputFile)
+    print(f"successfully saved report to {params.outputFile}")
 
 
 if __name__ == '__main__':
